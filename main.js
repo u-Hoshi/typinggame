a6fae053c755a01ba1286d00630be40433c19746
u-Hoshi <yuta000707@yahoo.co.jp>
2020-12-11 15:01:45 +0900

    完成

--- main.js
+++ main.js
@@ -1,31 +1,104 @@
+let num = 0
+var array = []
+// const messages = ['soccer', 'baseball', 'basketball','tennis'];
+// var messageNo = Math.floor( Math.random() * messages.length);
+reset()
+// console.log(messages[messageNo]);
+const body= document.querySelector("body")
+const doc = document.createElement("p")
+// messages[messageNo].charAt(num).style.fontSize-'red'
+doc.innerHTML = (messages[messageNo].substr(0,num))+"<span style= 'color:red';>"+(messages[messageNo]).charAt(num)+"</span>"+(messages[messageNo].slice(num+1))
+// doc.innerHTML = (messages[messageNo].slice(num,7))a
+// doc.createTextnode.charAt(num).style.co"lor="red"
+// doc.messages[messageNo].charAt(num).style.color="red"
+const now2 = 0
+body.appendChild(doc)
+
+// お題の下
+const now = document.createElement("p")
+now.textContent = (messages[messageNo]).charAt(num)
+now.style.color='red'
+body.appendChild(now)
+// お題の下end
+
+console.log(num)
 // キー入力受け取り
-    var num =0
 window.addEventListener("keydown", function (event) {
   let str = event.key
-    console.log(str)
+  array.push(event.key);
+  let strnum = array.join("")
+  console.log(str);
+  // const messages = ['soccer', 'baseball', 'basketball'];
+  // var messageNo = Math.floor( Math.random() * messages.length);
+  console.log(messages[messageNo]);
+  // console.log(num);
   
+
     // var num =0
-    if(str == messages[messageNo].charAt(num)) {
+  // 
+
+  // 
+  
+  
+  // if (str == messages[messageNo].charAt(messages[messageNo].length - 1)) {
+  //   reset();
+  // };
+  // let num =1
+  if (str === messages[messageNo].charAt(num)) {
         // num = num + 1
          // console.log("true");
-         num++;
-         console.log(num)
-    }
-           else {
-           console.log('false');
-        };
-    console.log(messages[messageNo].charAt(messages[messageNo].length - 1));
+    num++;
+    now.innerHTML = (messages[messageNo]).charAt(num)
+    doc.innerHTML = (messages[messageNo].substr(0,num))+"<span style= 'color:red';>"+(messages[messageNo]).charAt(num)+"</span>"+(messages[messageNo].slice(num+1))
+    console.log(num);
+    console.log(strnum)
+    console.log("正解")
+
+    if (strnum.length == messages[messageNo].length){
+      // if (str == messages[messageNo].charAt(messages[messageNo].length - 1)) {
+    // if (messages[messageNo].charAt(messages[messageNo].slice(-1))) {
+      let num = 0;
+        console.log(num);
+      console.log("終わり");
+      array.length = 0;
+        // let messageNo = Math.floor(Math.random() * messages.length);
+        // console.log(messages[messageNo]);
+        // console.log(messages[messageNo].charAt(num));
+        reset()
+      doc.innerHTML = (messages[messageNo].substr(0,num))+"<span style= 'color:red';>"+(messages[messageNo]).charAt(num)+"</span>"+(messages[messageNo].slice(num+1))
+      // body.appendChild(doc)
+      now.innerHTML = (messages[messageNo]).charAt(num)
+      }
+      
+    // } else if(str == messages[messageNo].charAt(messages[messageNo].length - 2)) {
+    //   // var messageNo = Math.floor( Math.random() * messages.length);
+    //   console.log("おわり")
+    }else {
+    console.log('間違い');
+    array.pop()
+    console.log(strnum)
+      };
+  
+    // console.log(messages[messageNo].charAt(messages[messageNo].length - 1));
 },
   true);
 //  check()
 
-const soccer ="soccer"
 
 // お題を出力
 
-const messages = ['soccer', 'baseball', 'basketball'];
-const messageNo = Math.floor( Math.random() * messages.length);
-console.log(messages[messageNo]);
+// const messages = ['soccer', 'baseball', 'basketball'];
+// var messageNo = Math.floor( Math.random() * messages.length);
+// console.log(messages[messageNo]);
+
+function reset() {
+  console.log("リセット")
+  messages = ['sepaktakraw', 'kabaddi', 'modernpentathlon','archery','cycling','triathlon','fencing'];
+  messageNo = Math.floor(Math.random() * messages.length);
+  console.log(messages[messageNo])
+  num = 0
+  console.log(num);
+};
 
 
 // 正誤判断(仮)
@@ -68,4 +141,6 @@ console.log(messages[messageNo]);
 // }
 
 
+// game()
 
+// 
